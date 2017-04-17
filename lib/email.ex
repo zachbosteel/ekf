385b3806366e589bd5a270a41d0f1632b7364964
zbosteel <zbosteel@gmail.com>
2017-04-16 22:24:24 -0500

    fix the email addresses

--- lib/email.ex
+++ lib/email.ex
@@ -3,7 +3,7 @@ defmodule Ekf.Email do
 
   def contact_form_email(email) do
     new_email()
-    |> to({"EKF Martial Arts", "zbosteel@gmail.com"})
+    |> to({"EKF Martial Arts", "extremekungfu@gmail.com"})
     |> from({"#{email.first_name} #{email.last_name}", "inquiries@ekfmartialarts.com"})
     |> subject(email.subject)
     |> assign(:email, email)
