8404781ef672df6856f9e2e59cbb03f7a00657f2
zbosteel <zbosteel@gmail.com>
2017-05-21 19:35:04 -0500

    add reply-to header

--- lib/email.ex
+++ lib/email.ex
@@ -7,6 +7,7 @@ defmodule Ekf.Email do
     |> from({"#{email.first_name} #{email.last_name}", "inquiries@ekfmartialarts.com"})
     |> subject("Contact Form Submission - #{email.subject}")
     |> assign(:email, email)
+    |> put_header("Reply-To", email.email_address)
     |> put_html_layout({Ekf.LayoutView, "email.html"})
     |> render("contact.html")
   end
